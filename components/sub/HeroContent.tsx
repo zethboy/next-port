4e85a606f85376fd39f52c03f4e8bf8285bc3b02
zethboy <yoginugraha842@gmail.com>
2025-03-18 14:48:10 +0700

    update

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from 'react';
+import {motion} from 'framer-motion';
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion';
+import { SparklesIcon } from '@heroicons/react/16/solid';
+import Image from "next/image"
+const HeroContent = () => {
+  return (
+    <motion.div
+    initial='hidden'
+    animate='visible'
+    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'>
+      <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+        <motion.div
+        variants={slideInFromTop}
+        className='Welcome-box py-[8px] px-[4x] border border-[#7042f88b] opacity-[0.9]'>
+
+          <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+          <h1 className='Welcome-text text-[32px]'>Giogi Web Development</h1>
+        </motion.div>
+        <motion.div
+        variants={slideInFromRight(0.5)}
+        className='flex flex-col gap-6 mt-6 text-6xl text-white font-bold max-w-[600px] w-auto h-auto'>
+          Providing
+          <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>
+          project experiance
+        </motion.div>
+        <motion.p
+        variants={slideInFromLeft(0.8)}
+        className='text-lg text-gray-400 my-5 max-w-[600px]'>
+          I&apos;m a Fullstack Software Engineer with experiance in Website, Mobile, and Software Development. Check out my projects and skills.
+        </motion.p>
+        <motion.a
+        variants={slideInFromLeft(1)}
+        className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'>
+          Learn More!
+        </motion.a>
+      </div>
+      <motion.div
+      variants={slideInFromRight(0.8)}
+      className='w-full h-full flex justify-center items-center'>
+        <Image
+         src="/mainIconsdark.svg"
+         alt="work icons"
+         height={650}
+         width={650}
+         />
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default HeroContent
