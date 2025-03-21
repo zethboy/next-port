ba1a28c0d3fe4a3ec24883f05455803f57800c1b
zethboy <yoginugraha842@gmail.com>
2025-03-21 16:22:57 +0700

    star bg

---
+++ components/main/StarBackground.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useState, useRef, Suspense } from "react"
+import React from 'react'
+import {Canvas, useFrame} from '@react-three/fiber'
+import { Points, PointMaterial, Preload } from "@react-three/drei"
+// import * as random from "maath/random/dist/maath-random.esm"
+
+const StarBackground = () => {
+  return (
+    <div>StarBackground</div>
+  )
+}
+
+export default StarBackground
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -14,7 +14,7 @@ const HeroContent = () => {
       <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
         <motion.div
         variants={slideInFromTop}
-        className='Welcome-box py-[8px] px-[4x] border border-[#7042f88b] opacity-[0.9]'>
+        className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
 
           <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
           <h1 className='Welcome-text text-[32px]'>Giogi Web Development</h1>
