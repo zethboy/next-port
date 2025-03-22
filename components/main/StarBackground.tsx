4468a6b8d0719ded324e356351e8c9adeb79c948
zethboy <yoginugraha842@gmail.com>
2025-03-22 19:04:52 +0700

    canvas

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -18,8 +18,33 @@ const StarBackground = (props: any) => {
     ref.current.rotaion.y -= delta/15;
   })
   return (
-    <div>StarBackground</div>
+    <group rotation={[0, 0, Math.PI /4]}>
+      <Points 
+      ref={ref}
+      positions={sphere}
+      stride={3}
+      frustumCulled 
+      {...props}>
+        <PointMaterial
+         transparent
+         color="$fff" 
+         size={0.002}
+         sizeAttenuation={true}
+         depthWrite={false}/>
+      </Points>
+    </group>
   )
 }
+
+const StartCanvas = () => (
+  <div className="w-full h-auto fixed inset-0 z-[20]">
+    <Canvas>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+
+)
 
 export default StarBackground
