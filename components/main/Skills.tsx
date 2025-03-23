1061bfd138906d0b6f20dccfcae3e399dd3a09ee
zethboy <yoginugraha842@gmail.com>
2025-03-23 22:06:35 +0700

    add skills

---
+++ components/main/Skills.tsx
@@ -0,0 +1,15 @@
+import { transform } from 'next/dist/build/swc/generated-native'
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section 
+    id='skills'
+    style={{transform : "scale(0.9)"}}
+    className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'>
+
+    </section>
+  )
+}
+
+export default Skills
