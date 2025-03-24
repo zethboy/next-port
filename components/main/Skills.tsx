9bc6e7c9b283158ab9e05987e040954407ea8a30
zethboy <yoginugraha842@gmail.com>
2025-03-24 18:48:51 +0700

    absen

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
     id='skills'
     style={{transform : "scale(0.9)"}}
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'>
-
+      <div>absen</div>
     </section>
   )
 }
