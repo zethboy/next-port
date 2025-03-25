a9acf5ea922705c9b873375bce2241605cf20c75
zethboy <yoginugraha842@gmail.com>
2025-03-25 19:49:47 +0700

    skills section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/Skills";
 
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col h-[800px] gap-20">
         <Hero />
+        <Skills />
       </div>
     </main>
   );
--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,5 +1,7 @@
+import { Frontend_skill } from '@/constants'
 import { transform } from 'next/dist/build/swc/generated-native'
 import React from 'react'
+import SkillDataProvider from '../sub/SkillDataProvider'
 
 const Skills = () => {
   return (
@@ -8,7 +10,15 @@ const Skills = () => {
     style={{transform : "scale(0.9)"}}
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'>
       <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
-
+        {Frontend_skill.map((image, index) => (
+          <SkillDataProvider 
+          key={index}
+           src={image.Image}
+           width={image.width}
+           height={image.height}
+           index={index}
+           />
+        ))}
       </div>
     </section>
   )
