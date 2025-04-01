c0fff1154b162cfc80e1dd4ed76ce4da5a042dad
zethboy <yoginugraha842@gmail.com>
2025-04-01 14:40:19 +0700

    commit

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SkillText = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center '>
-      <div>absen dulu</div>
+      
     </div>
   )
 }
