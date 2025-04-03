1e4bc1d94cca3f2ebad45801a33b94ee9a1e3875
zethboy <yoginugraha842@gmail.com>
2025-04-03 18:51:24 +0700

    skill

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,9 +1,19 @@
+"use client"
+import { motion } from 'framer-motion'
+import { slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
 const SkillText = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center '>
-       <div className=''></div>
+       <motion.div
+        variants={slideInFromTop}
+        className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
+
+          <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+          <h1 className='Welcome-text text-[22px] md:text-[32px]'>Giogi Web Development</h1>
+        </motion.div>
     </div>
   )
 }
