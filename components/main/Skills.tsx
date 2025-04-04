70480c0b1966b403789a4ff70c7542d66e0e9a42
zethboy <yoginugraha842@gmail.com>
2025-04-04 18:20:53 +0700

    hijau

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -2,6 +2,7 @@ import { Backend_skill, Frontend_skill, Full_stack, Other_skill } from '@/consta
 import { transform } from 'next/dist/build/swc/generated-native'
 import React from 'react'
 import SkillDataProvider from '../sub/SkillDataProvider'
+import SkillText from '../sub/SkillText'
 
 const Skills = () => {
   return (
@@ -9,6 +10,7 @@ const Skills = () => {
     id='skills'
     style={{transform : "scale(0.9)"}}
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'>
+      <SkillText />
       <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
         {Frontend_skill.map((image, index) => (
           <SkillDataProvider 
--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { motion } from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
@@ -12,7 +12,17 @@ const SkillText = () => {
         className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
 
           <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-          <h1 className='Welcome-text text-[22px] md:text-[32px]'>Giogi Web Development</h1>
+          <h1 className='Welcome-text text-[22px] md:text-[32px]'>Think better with Next Js 13</h1>
+        </motion.div>
+        <motion.div
+        variants={slideInFromLeft(0.5)}
+        className='text-[30px] text-white font-medium mt-[10] text-center mb-[15px]'>
+          Making apps with modern technologies
+        </motion.div>
+        <motion.div
+        variants={slideInFromRight(0.5)}
+        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'>
+          Never miss a task deadline or idea
         </motion.div>
     </div>
   )
