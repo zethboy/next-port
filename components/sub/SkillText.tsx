988613e0ecbe1eaec17f98f5b0cfedef6c799603
zethboy <yoginugraha842@gmail.com>
2025-04-10 19:05:21 +0700

    hijau

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -12,7 +12,7 @@ const SkillText = () => {
         className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
 
           <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-          <h1 className='Welcome-text text-[22px] md:text-[32px]'>Think better with Next JS 13</h1>
+          <h1 className='Welcome-text text-[22px] md:text-[32px]'>Think better with Next JS</h1>
         </motion.div>
         <motion.div
         variants={slideInFromLeft(0.5)}
